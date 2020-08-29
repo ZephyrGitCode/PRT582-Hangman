d5663a1cf5c98cac4c591603115c0fc755643df9
ZephyrGitCode <zephyr.dobson@outlook.com>
2020-08-29 20:13:25 +0930

    Completed test case 5

--- main_2.py
+++ main_2.py
@@ -1,23 +1,43 @@
 # Importing  required libraries
 import tkinter as tk
-from english_words import english_words_lower_set
+from english_words import english_words_set
 
+# Generates the random word
 def generateword():
-    words = list(english_words_lower_set)
-    randword = words[1].upper()
-    return randword
+    randword = list(english_words_set)[1].upper()
+    spacedword = ""
+    for letter in randword:
+        spacedword += letter
+        spacedword += " "
+    return spacedword
 
+# Generates a hidden list based on word
+def listword(word):
+    wordlist = []
+    for letter in word:
+        if letter != " ":
+            wordlist.append("_")
+        else:
+            wordlist.append(letter)
+    return wordlist
+
+# Generates a hidden string based on word
 def hideword(word):
     hiddenword = ""
     for letter in word:
-        hiddenword += "_ "
+        if letter != " ":
+            hiddenword += "_"
+        else:
+            hiddenword += letter
     return hiddenword
 
 word = generateword()
+wordlist = listword(word)
 hiddenword = hideword(word)
 
 print("Random word: " + word)
 print("Hidden word: " + hiddenword)
+
 # Main tkinter application
 class Applicatiion(tk.Frame):
     def __init__(self, master=None):
@@ -27,6 +47,8 @@ class Applicatiion(tk.Frame):
         self.hiddenword = tk.StringVar()
         self.hiddenword.set(hiddenword)
         self.guess = tk.StringVar()
+        self.rightlist = []
+        self.revealedword = ""
 
         self.pack()
         self.create_word()
@@ -53,6 +75,23 @@ class Applicatiion(tk.Frame):
         if self.entry.get() and self.entry.get().isalpha():
             self.letter = self.entry.get()[0].upper()
             self.guess.set(self.letter)
+            if self.letter in word and self.letter not in self.rightlist:
+                self.rightlist.append(self.letter)
+                self.revealedword = self.hiddenword.get()
+                # convert string to list
+                self.revealedwordlist = []
+                for letter in self.revealedword:
+                    self.revealedwordlist.append(letter)
+                # if current letter matches self.letter then reveal the letter
+                for letter in range(len(word)):
+                    if self.letter == word[letter]:
+                        self.revealedwordlist[letter] = self.letter
+                # converts list back to string
+                self.revealedword = ""
+                for letter in self.revealedwordlist:
+                    self.revealedword+=letter
+                # re-prints the revealed word to display
+                self.hiddenword.set(self.revealedword)
         else:
             self.guess.set("You must enter a letter")
         self.entry.delete(0, 'end')
