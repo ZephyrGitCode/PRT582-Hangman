d5adf4f9323818b338fb9832cb98e63a4ec41c48
ZephyrGitCode <zephyr.dobson@outlook.com>
2020-08-29 11:04:03 +0930

    Up to test case 3, user guess entry

--- main_2.py
+++ main_2.py
@@ -2,38 +2,47 @@
 import tkinter as tk
 from english_words import english_words_lower_set
 
-words = list(english_words_lower_set)
-word = words[1]
+def generateword():
+    words = list(english_words_lower_set)
+    randword = words[1].upper()
+    return randword
+
+def hideword(word):
+    hiddenword = ""
+    for letter in word:
+        hiddenword += "_ "
+    return hiddenword
+
+word = generateword()
+hiddenword = hideword(word)
+
 print("Random word: " + word)
-
-letter = tk.StringVar() 
-
+print("Hidden word: " + hiddenword)
 # Main tkinter application
 class Applicatiion(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
         self.master = master
+
+        self.hiddenword = tk.StringVar()
+        self.hiddenword.set(hiddenword)
+
         self.pack()
-        self.create_items()
+        self.create_word()
 
-
-
-    def create_items(self):
-        self.entrylabel = tk.Label(self, text="Enter a letter")
-        self.entry = tk.Entry(self, bd=5)
-
+    def create_word(self):
+        self.wordlabel = tk.Label(self, textvariable=self.hiddenword)
+        self.wordlabel.pack()
 
         #self.quit = tk.Button(self, text="QUIT", fg="red", command=self.master.destroy)
         #self.quit.pack(side="left")
 
-    def say_hi(self):
-        print("Hello0 wworld")
-
-    def get_entry(self):
+    
+    """ def get_entry(self):
         letter = self.entry.get()
-        print("Random letter screen = " + letter)
+        print("Random letter screen = " + letter) """
 
-# Launch window
+# Launch game window
 root = tk.Tk()
 root.title("Hangman Zephyr")
 root.geometry("300x225")
